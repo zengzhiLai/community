8f178ad5f7f1725ee806588be6cea5afd89368c9
zengzhiLai <l15767589823@163.com>
2019-05-29 17:14:40 +0800

    add index.html and bootstrap

--- src/main/java/cn/lnsf/community/community/controller/HelloController.java
+++ src/main/java/cn/lnsf/community/community/controller/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
     @GetMapping("/hello")//找到templates下的hello.html
     public String hello(@RequestParam(name = "name") String name, Model model) {
         model.addAttribute("name",name);
-        return "hello";
+        return "index";
     }
 
 }
