003c3b0f0a7f7a1e8c3638fa13be9c6b003adb0f
zengzhiLai <l15767589823@163.com>
2019-09-18 16:07:34 +0800

    Add check cookies

--- src/main/java/cn/lnsf/community/community/controller/IndexController.java
+++ src/main/java/cn/lnsf/community/community/controller/IndexController.java
@@ -1,8 +1,13 @@
 package cn.lnsf.community.community.controller;
 
+import cn.lnsf.community.community.mapper.UserMapper;
+import cn.lnsf.community.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author ：赖增智
@@ -10,8 +15,24 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class IndexController {
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+
+        }
+
         return "index";
     }
 
