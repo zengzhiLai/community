3bb5033b28f002432e4d5a83de6353784116eb19
zengzhiLai <l15767589823@163.com>
2019-09-19 16:31:18 +0800

    Add flyway migration support

--- src/main/java/cn/lnsf/community/community/controller/AuthorizeController.java
+++ src/main/java/cn/lnsf/community/community/controller/AuthorizeController.java
@@ -1,10 +1,10 @@
 package cn.lnsf.community.community.controller;
 
+import cn.lnsf.community.community.dto.AccessTokenDTO;
+import cn.lnsf.community.community.dto.GithubUser;
 import cn.lnsf.community.community.mapper.UserMapper;
 import cn.lnsf.community.community.model.User;
 import cn.lnsf.community.community.provider.GithubProvider;
-import cn.lnsf.community.community.dto.AccessTokenDTO;
-import cn.lnsf.community.community.dto.GithubUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
@@ -41,7 +40,6 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request,
                            HttpServletResponse response) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
