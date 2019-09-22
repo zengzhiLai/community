76a60d3e865eedcdd97ce2425a2259187fe6c537
zengzhiLai <l15767589823@163.com>
2019-09-22 14:35:26 +0800

    Add the support of Lombok

--- src/main/java/cn/lnsf/community/community/controller/PublishController.java
+++ src/main/java/cn/lnsf/community/community/controller/PublishController.java
@@ -34,9 +34,9 @@ public class PublishController {
 
     @PostMapping("/publish")
     public String doPublish(
-            @RequestParam(value = "title",required = false) String title,
-            @RequestParam(value = "description",required = false) String description,
-            @RequestParam(value = "tag",required = false) String tag,
+            @RequestParam(value = "title", required = false) String title,
+            @RequestParam(value = "description", required = false) String description,
+            @RequestParam(value = "tag", required = false) String tag,
             HttpServletRequest request,
             Model model) {
 
@@ -60,16 +60,17 @@ public class PublishController {
         //判断用户是否登录
         User user = null;
         Cookie[] cookies = request.getCookies();
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("token")) {
-                String token = cookie.getValue();
-                user = userMapper.findByToken(token);
-                if (user != null) {
-                    request.getSession().setAttribute("user", user);
+        if (cookies != null && cookies.length != 0)
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
                 }
-                break;
             }
-        }
 
         if (user == null) {
             model.addAttribute("error", "用户未登录");
--- src/main/java/cn/lnsf/community/community/dto/GithubUser.java
+++ src/main/java/cn/lnsf/community/community/dto/GithubUser.java
@@ -1,44 +1,16 @@
 package cn.lnsf.community.community.dto;
 
+import lombok.Data;
+
 /**
+ * https://api.github.com/users/zengzhilai
  * @author ：赖增智
  * @date ：Created in 2019-6-1 22:14
  */
+@Data
 public class GithubUser {
     private String name;
     private Long id;
     private String bio;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
-
-    @Override
-    public String toString() {
-        return "GithubUser{" +
-                "name='" + name + '\'' +
-                ", id=" + id +
-                ", bio='" + bio + '\'' +
-                '}';
-    }
+    private String avatar_url; //头像地址
 }
